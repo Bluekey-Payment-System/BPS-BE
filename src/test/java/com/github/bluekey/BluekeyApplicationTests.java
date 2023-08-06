655bec055376e505a8a69f1e68b305bdbbb7aa21
oereo <dlstpgns0406@gmail.com>
2023-08-06 12:46:42 +0900

    chore: project base config

---
+++ src/test/java/com/github/bluekey/BluekeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.bluekey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluekeyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
