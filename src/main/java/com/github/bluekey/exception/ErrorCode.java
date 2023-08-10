36416a70a444877f4b75efce64946bb4cf08b08c
oereo <dlstpgns0406@gmail.com>
2023-08-10 15:06:47 +0900

    feat: Invalid input value or type에 대한 exception 정의와 handler 추가

--- src/main/java/com/github/bluekey/exception/ErrorCode.java
+++ src/main/java/com/github/bluekey/exception/ErrorCode.java
@@ -10,7 +10,11 @@ public enum ErrorCode {
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "S_001", "서버에 오류가 발생했습니다."),
 
     AUTHORIZATION_FAILED(HttpStatus.UNAUTHORIZED, "AR_001", "권한이 없습니다."),
-    AUTHENTICATION_FAILED(HttpStatus.FORBIDDEN, "AU_001", "인증에 실패하였습니다.");
+    AUTHENTICATION_FAILED(HttpStatus.FORBIDDEN, "AU_001", "인증에 실패하였습니다."),
+
+    // 400~
+    INVALID_INPUT_TYPE(HttpStatus.BAD_REQUEST, "C_001", "입력값의 타입이 유효하지 않습니다."),
+    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "C_002", "입력값이 유효하지 않습니다.");
 
     private final HttpStatus status;
     private final String code;
--- src/main/java/com/github/bluekey/exception/GlobalExceptionHandler.java
+++ src/main/java/com/github/bluekey/exception/GlobalExceptionHandler.java
@@ -2,8 +2,14 @@ package com.github.bluekey.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestControllerAdvice
@@ -20,6 +26,23 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleAuthorizationException(AuthorizationException ex) {
         ErrorResponse response = ErrorResponse.of(ErrorCode.AUTHORIZATION_FAILED);
         log.debug("Request does not have authorization: {}", ex.getDetail());
+        return new ResponseEntity<>(response, response.getStatus());
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex) {
+        ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_TYPE);
+        log.debug("Parameter type is invalid: {}", ex.getMessage());
+        return new ResponseEntity<>(response, response.getStatus());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> fieldErrors = ex.getBindingResult().getFieldErrors().stream()
+                .collect(Collectors.toMap(FieldError::getField, error -> error.getDefaultMessage() != null ? error.getDefaultMessage() : ""));
+
+        ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE, fieldErrors);
+        log.debug("Argument validation has failed: {}", ex.getMessage());
         return new ResponseEntity<>(response, response.getStatus());
     }
 }
