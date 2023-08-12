8ba7b517b5c6d18cc6cee4a367e497d8565d3dc9
oereo <dlstpgns0406@gmail.com>
2023-08-12 10:19:06 +0900

    feat: 리스트 타입의 데이터 반환 목적에 사용할 ListResponse dto class 추가

---
+++ src/main/java/com/github/bluekey/dto/response/ListResponse.java
@@ -0,0 +1,20 @@
+package com.github.bluekey.dto.response;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ListResponse<T> {
+    private int totalItems;
+    private List<T> contents = new ArrayList<>();
+
+    public ListResponse(int totalItems, List<T> contents) {
+        this.totalItems = totalItems;
+        this.contents = contents;
+    }
+}
