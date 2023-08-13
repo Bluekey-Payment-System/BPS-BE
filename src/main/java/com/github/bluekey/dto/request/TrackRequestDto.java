743e50d2bbaedbf837f7f2e7b7e67865ced3a4ce
김도완 <kim.raon930319@gmail.com>
2023-08-14 05:18:46 +0900

    TrackDTO 수정

--- src/main/java/com/github/bluekey/dto/request/TrackRequestDto.java
+++ src/main/java/com/github/bluekey/dto/request/TrackRequestDto.java
@@ -1,4 +1,25 @@
 package com.github.bluekey.dto.request;
 
+import com.github.bluekey.dto.TrackCommissionRateDto;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
 public class TrackRequestDto {
+    @Schema(description = "한글 트랙명", example = "아이엠")
+    private final String koTrackName;
+
+    @Schema(description = "영문 트랙명", example = "I AM")
+    private final String enTrackName;
+
+    @Schema(description = "???", example = "false")
+    private final boolean isOriginalTrack;
+
+    @Schema(description = "트랙별 요율정보")
+    private final List<TrackCommissionRateDto> participants;
+
 }
