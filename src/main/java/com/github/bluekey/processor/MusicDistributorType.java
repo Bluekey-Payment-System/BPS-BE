07bc1e2f60cdb4eacf822813b1c035f6dce80349
oereo <dlstpgns0406@gmail.com>
2023-08-15 00:21:39 +0900

    refactor: MusicDistributorType enum class의 형식을 정규식과 provider class 형식으로 매핑

--- src/main/java/com/github/bluekey/processor/MusicDistributorType.java
+++ src/main/java/com/github/bluekey/processor/MusicDistributorType.java
@@ -4,13 +4,15 @@ import lombok.Getter;
 
 @Getter
 public enum MusicDistributorType {
-    ATO("L2110A11_\\d{6}$"),
-    THREE_POINT_ONE_FOUR("^\\d{6}-[a-zA-Z0-9]+$"),
-    MAFIA("마피아뮤직_\\d{6}\\$");
+    ATO("L2110A11_\\d{6}$", AtoDistributorExcelFileProvider.class),
+    THREE_POINT_ONE_FOUR("^\\d{6}-[a-zA-Z0-9]+$", AtoDistributorExcelFileProvider.class),
+    MAFIA("마피아뮤직_\\d{6}\\$", AtoDistributorExcelFileProvider.class);
 
     private final String pattern;
+    private final Class<?> cls;
 
-    MusicDistributorType(String pattern) {
+    MusicDistributorType(String pattern, Class<?> cls ) {
         this.pattern = pattern;
+        this.cls = cls;
     }
 }
