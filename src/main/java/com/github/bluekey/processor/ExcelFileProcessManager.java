fa3697686a0c73139249aa30ed9257dfe7f81217
oereo <dlstpgns0406@gmail.com>
2023-08-15 00:28:51 +0900

    feat: ExcelFileProcessManager를 통해 엑셀파일 타입에 따른 provider 선택 로직 추가

---
+++ src/main/java/com/github/bluekey/processor/ExcelFileProcessManager.java
@@ -0,0 +1,46 @@
+package com.github.bluekey.processor;
+
+import lombok.Getter;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Getter
+public class ExcelFileProcessManager {
+    private static final String FILE_SEPARATOR = ".";
+    private final MultipartFile file;
+    private final ExcelFileProvider excelFileProvider;
+    private String filetype;
+    private String fileName;
+
+    public ExcelFileProcessManager(MultipartFile file) {
+        setExcelFileBasicInformation(file);
+        this.excelFileProvider = setProvider();
+        this.file = file;
+    }
+
+    private void setExcelFileBasicInformation(MultipartFile file) {
+        String[] originalFileNameInformation = file.getOriginalFilename().split(FILE_SEPARATOR);
+        this.filetype = originalFileNameInformation[0];
+        this.fileName = originalFileNameInformation[1];
+    }
+
+    private ExcelFileProvider setProvider() {
+        for (MusicDistributorType type : MusicDistributorType.values()) {
+            Pattern pattern = Pattern.compile(type.getPattern());
+            Matcher matcher = pattern.matcher(fileName);
+            if (matcher.matches()) {
+                return determineExcelFileProviderWithMusicDistributorType(type);
+            }
+        }
+        throw new IllegalArgumentException("Unsupported file");
+    }
+
+    private ExcelFileProvider determineExcelFileProviderWithMusicDistributorType(MusicDistributorType type) {
+        if (type.getCls().equals(AtoDistributorExcelFileProvider.class)) {
+            return new AtoDistributorExcelFileProvider(file);
+        }
+        throw new IllegalArgumentException("Excel File Provider exception");
+    }
+}
