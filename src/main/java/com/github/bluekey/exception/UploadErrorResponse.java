edb9b2ecb252fcb0d9c855b4f669aaa145329f2b
oereo <dlstpgns0406@gmail.com>
2023-08-17 14:02:45 +0900

    feat: Swagger description 추가

--- src/main/java/com/github/bluekey/exception/UploadErrorResponse.java
+++ src/main/java/com/github/bluekey/exception/UploadErrorResponse.java
@@ -2,6 +2,7 @@ package com.github.bluekey.exception;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.github.bluekey.processor.ExcelRowException;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,12 +14,14 @@ import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Schema(description = "엑셀 파일 업로드 시에 반환하는 ErrorResponse 타입")
 public class UploadErrorResponse {
 
     @JsonIgnore
     private HttpStatus status;
     private String code;
     private String message;
+    @Schema(description = "엑셀 파일 기준으로 총 에러가 발생한 행의 갯수", example = "10")
     private int totalErrorNums;
     private List<ExcelRowException> errors = new ArrayList<>();
 
