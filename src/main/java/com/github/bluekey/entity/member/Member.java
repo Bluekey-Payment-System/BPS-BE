b78f70501d8b19b6d7d863d3a2317692341aec10
oereo <dlstpgns0406@gmail.com>
2023-08-17 22:31:53 +0900

    feat: validateCommissionRate 검증 method 적용과 생성자 형식 변경

--- src/main/java/com/github/bluekey/entity/member/Member.java
+++ src/main/java/com/github/bluekey/entity/member/Member.java
@@ -1,12 +1,9 @@
 package com.github.bluekey.entity.member;
 
 import com.github.bluekey.entity.BaseTimeEntity;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+
+import javax.persistence.*;
+
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,13 +14,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "member")
 public class Member extends BaseTimeEntity {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "member_id")
 	private Long id;
 
-	@Column(name = "email", nullable = false, unique = true)
-	private String email;
+	@Embedded
+	private Email email;
 
 	@Column(name = "login_id", nullable = false, unique = true)
 	private String loginId;
@@ -42,38 +40,49 @@ public class Member extends BaseTimeEntity {
 	private Integer commissionRate;
 
 	@Column(name = "type", nullable = false)
+	@Enumerated(EnumType.STRING)
 	private MemberType type;
 
 	@Column(name = "role", nullable = false)
+	@Enumerated(EnumType.STRING)
 	private MemberRole role;
 
 	@Column(name = "profile_image")
 	private String profileImage;
 
 	@Builder(builderClassName = "ByArtistBuilder", builderMethodName = "ByArtistBuilder")
-	public Member(String email, String loginId, String password, String name, String enName, Integer commissionRate,
-			MemberType type, MemberRole role, String profileImage) {
-		this.email = email;
+	public Member(String email, String loginId, String password, String name, String enName, Integer commissionRate, String profileImage) {
+		validateCommissionRate(commissionRate);
+		this.email = new Email(email);
 		this.loginId = loginId;
 		this.password = password;
 		this.name = name;
 		this.enName = enName;
 		this.commissionRate = commissionRate;
-		this.type = type;
-		this.role = role;
+		this.type = MemberType.USER;
+		this.role = MemberRole.ARTIST;
 		this.profileImage = profileImage;
 	}
 
 	@Builder(builderClassName = "ByAdminBuilder", builderMethodName = "ByAdminBuilder")
-	public Member(String email, String loginId, String password, String name,
-			MemberType type, MemberRole role, String profileImage) {
-		this.email = email;
+	public Member(String email, String loginId, String password, String name, MemberRole role, String profileImage) {
+		this.email = new Email(email);;
 		this.loginId = loginId;
 		this.password = password;
 		this.name = name;
 		this.commissionRate = 0;
-		this.type = type;
+		this.type = MemberType.ADMIN;
 		this.role = role;
 		this.profileImage = profileImage;
+	}
+
+	private void validateCommissionRate(Integer commissionRate) {
+		if (commissionRate < 0) {
+			throw new IllegalArgumentException("Percentage value must not be negative.");
+		}
+
+		if (commissionRate > 100) {
+			throw new IllegalArgumentException("Percentage value cannot exceed 100.");
+		}
 	}
 }
