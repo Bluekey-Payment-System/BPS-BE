57b83238bec7ac9445d84aba98fea584b4ffca24
oereo <dlstpgns0406@gmail.com>
2023-08-18 18:58:50 +0900

    feat: 각 JpaRepository에 필요한 method 정의

--- src/main/java/com/github/bluekey/repository/member/MemberRepository.java
+++ src/main/java/com/github/bluekey/repository/member/MemberRepository.java
@@ -3,7 +3,12 @@ package com.github.bluekey.repository.member;
 import com.github.bluekey.entity.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 	boolean existsByEmail(String email);
 	boolean existsByLoginId(String loginId);
+
+	Optional<Member> findMemberByEnName(String enName);
+	Optional<Member> findMemberByName(String name);
 }
--- src/main/java/com/github/bluekey/repository/track/TrackMemberRepository.java
+++ src/main/java/com/github/bluekey/repository/track/TrackMemberRepository.java
@@ -3,5 +3,8 @@ package com.github.bluekey.repository.track;
 import com.github.bluekey.entity.track.TrackMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TrackMemberRepository extends JpaRepository<TrackMember, Long> {
+    Optional<TrackMember> findTrackMemberByName(String name);
 }
