01b3f08ef9441658ba803480cbcc50d9b1a24749
eunbi9n <gombi9_9@naver.com>
2023-08-18 19:18:45 +0900

    feat: spring security 사용을 위한 securityConfig 설정

--- src/main/java/com/github/bluekey/config/SecurityConfig.java
+++ src/main/java/com/github/bluekey/config/SecurityConfig.java
@@ -1,22 +1,62 @@
 package com.github.bluekey.config;
 
 import com.github.bluekey.jwt.JwtAuthenticationFilter;
+import com.github.bluekey.jwt.JwtProvider;
+import java.util.List;
+import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+	private final JwtProvider jwtProvider;
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+				.httpBasic().disable()
+				.csrf().disable()
+				.cors(c -> {
+					c.configurationSource(request -> {
+						CorsConfiguration config = new CorsConfiguration();
+						config.setAllowedOrigins(List.of("*")); //TODO: front 주소로 변경 필요
+						config.setAllowedMethods(List.of("GET", "POST", "PATCH", "DELETE"));
+						return config;
+					});
+				})
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and()
+				.authorizeRequests()
+				.antMatchers("/api/v1/auth/admin/**", "api/v1/auth/member/login").permitAll()
+				.antMatchers("/api/v1/admin/**").hasRole("ADMIN")
+				.anyRequest().authenticated()
+				.and()
+				.addFilterBefore(new JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class)
+				.exceptionHandling()
+				.accessDeniedHandler(
+						(request, response, accessDeniedException) -> response.sendError(HttpServletResponse.SC_FORBIDDEN)
+				)
+				.authenticationEntryPoint(
+						(request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED)
+				);
+		return http.build();
+	}
 }
