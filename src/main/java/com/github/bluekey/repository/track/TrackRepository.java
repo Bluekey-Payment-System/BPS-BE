d88468310d29c8bc2d4e4dd9498a3b5db6f6a7fb
oereo <dlstpgns0406@gmail.com>
2023-08-19 00:17:35 +0900

    feat: TrackRepository method 정의

--- src/main/java/com/github/bluekey/repository/track/TrackRepository.java
+++ src/main/java/com/github/bluekey/repository/track/TrackRepository.java
@@ -3,5 +3,10 @@ package com.github.bluekey.repository.track;
 import com.github.bluekey.entity.track.Track;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TrackRepository extends JpaRepository<Track, Long> {
+
+    List<Track> findTracksByName(String name);
+    List<Track> findTracksByEnName(String enName);
 }
