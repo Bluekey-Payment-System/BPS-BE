52fff916c1e3e8aab90734408677254b04705ae5
eunbi9n <gombi9_9@naver.com>
2023-08-18 15:17:58 +0900

    feat: jwtProvider 구현

--- src/main/java/com/github/bluekey/jwt/JwtProvider.java
+++ src/main/java/com/github/bluekey/jwt/JwtProvider.java
@@ -1,6 +1,7 @@
 package com.github.bluekey.jwt;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -8,10 +9,15 @@ import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -24,6 +30,8 @@ public class JwtProvider {
 
 	@Value("${jwt.token-validity-in-seconds}")
 	private Long tokenValidMilisecond;
+
+	private final UserDetailsService userDetailsService;
 
 	@PostConstruct
 	protected void init() {
@@ -41,4 +49,35 @@ public class JwtProvider {
 				.compact();
 	}
 
+	public Authentication getAuthentication (String token) {
+		UserDetails userDetails = userDetailsService.loadUserByUsername(this.getAccount(token));
+		return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+	}
+
+	public String resolveToken(HttpServletRequest request) {
+		return request.getHeader("Authorization");
+	}
+
+	public boolean validateToken(String token) {
+		try {
+			if (!token.substring(0, "BEARER ".length()).equalsIgnoreCase("BEARER ")) {
+				return false;
+			} else {
+				token = token.split(" ")[1].trim();
+			}
+			Jws<Claims> claims = Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token);
+			return !claims.getBody().getExpiration().before(new Date());
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	public String getAccount(String token) {
+		return Jwts.parserBuilder()
+				.setSigningKey(secretKey)
+				.build()
+				.parseClaimsJws(token)
+				.getBody()
+				.getSubject();
+	}
 }
