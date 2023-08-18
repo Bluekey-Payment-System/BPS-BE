5ce033fcd528922df173e89258a7b716fd32ac36
김도완 <kim.raon930319@gmail.com>
2023-08-19 07:27:12 +0900

    refactor:누락 API 추가 및 정정

--- src/main/java/com/github/bluekey/dto/response/ArtistListReponseDto.java
+++ src/main/java/com/github/bluekey/dto/response/ArtistListReponseDto.java
@@ -1,23 +1,19 @@
 package com.github.bluekey.dto.response;
 
-import com.github.bluekey.dto.AlbumMonthlyInfoDto;
-import com.github.bluekey.dto.AlbumMonthlySettlementInfoDto;
-import com.github.bluekey.dto.AlbumMonthlyTrackInfoDto;
+import com.github.bluekey.dto.*;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
-
-import java.util.List;
 
 @Getter
 @Builder
 public class ArtistListReponseDto {
     @Schema(description = "당월 정산 정보")
-    private final List<AlbumMonthlySettlementInfoDto> settlement;
+    private final ArtistMonthlySettlementInfoDto settlement;
 
     @Schema(description = "당월 앨범 정보")
-    private final List<AlbumMonthlyInfoDto> bestAlbum;
+    private final ArtistMonthlyInfoDto bestAlbum;
 
     @Schema(description = "당월 트랙 정보")
-    private final List<AlbumMonthlyTrackInfoDto> bestTrack;
+    private final ArtistMonthlyTrackInfoDto bestTrack;
 }
