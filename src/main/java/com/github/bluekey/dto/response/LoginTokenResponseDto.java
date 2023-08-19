d64919468aaaa2eb47d972f28f83e8b8a4b190a1
eunbi9n <gombi9_9@naver.com>
2023-08-19 20:45:34 +0900

    fix: admin login 구현

--- src/main/java/com/github/bluekey/dto/response/LoginTokenResponseDto.java
+++ src/main/java/com/github/bluekey/dto/response/LoginTokenResponseDto.java
@@ -3,16 +3,22 @@ package com.github.bluekey.dto.response;
 import com.github.bluekey.dto.JwtInfoDto;
 import com.github.bluekey.dto.LoginMemberDto;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
-@Builder
-public class LoginTokenResqonseDto {
+public class LoginTokenResponseDto {
 	@Schema(description = "로그인 회원 정보")
 	private final LoginMemberDto member;
 	@Schema(description = "jwt 정보")
 	private final JwtInfoDto jwtInformation;
+
+	@Builder
+	public LoginTokenResponseDto(LoginMemberDto member, JwtInfoDto jwtInformation) {
+		this.member = member;
+		this.jwtInformation = jwtInformation;
+	}
 }
