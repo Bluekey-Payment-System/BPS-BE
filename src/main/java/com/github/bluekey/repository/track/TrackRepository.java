7890762fbe102c2d50dfa0408e490057ed3fcf3c
oereo <dlstpgns0406@gmail.com>
2023-08-19 19:10:45 +0900

    chore: findTrackMemberByNameAndTrack method와 findTrackByNameIgnoreCase, findTrackByEnNameIgnoreCase 정의

--- src/main/java/com/github/bluekey/repository/track/TrackRepository.java
+++ src/main/java/com/github/bluekey/repository/track/TrackRepository.java
@@ -4,11 +4,13 @@ import com.github.bluekey.entity.album.Album;
 import com.github.bluekey.entity.track.Track;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface TrackRepository extends JpaRepository<Track, Long> {
 
     Optional<Track> findTrackByNameIgnoreCaseAndAlbum(String name, Album album);
     Optional<Track> findTrackByEnNameIgnoreCaseAndAlbum(String enName, Album album);
+
+    Optional<Track> findTrackByNameIgnoreCase(String name);
+    Optional<Track> findTrackByEnNameIgnoreCase(String enName);
 }
