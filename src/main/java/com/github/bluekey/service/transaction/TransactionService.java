862895872e1532ad8164b893c32e7a3f21d4811f
oereo <dlstpgns0406@gmail.com>
2023-08-20 13:45:40 +0900

    feat: S3에 검증이 끝난 excel file을 업로드 하는 로직 추가

--- src/main/java/com/github/bluekey/service/transaction/TransactionService.java
+++ src/main/java/com/github/bluekey/service/transaction/TransactionService.java
@@ -1,5 +1,6 @@
 package com.github.bluekey.service.transaction;
 
+import com.amazonaws.services.s3.model.S3Object;
 import com.github.bluekey.dto.request.transaction.OriginalTransactionRequestDto;
 import com.github.bluekey.dto.response.ListResponse;
 import com.github.bluekey.dto.response.transaction.OriginalTransactionResponseDto;
@@ -12,10 +13,15 @@ import com.github.bluekey.repository.member.MemberRepository;
 import com.github.bluekey.repository.track.TrackMemberRepository;
 import com.github.bluekey.repository.track.TrackRepository;
 import com.github.bluekey.repository.transaction.OriginalTransactionRepository;
+import com.github.bluekey.s3.manager.AwsS3Manager;
+import com.github.bluekey.s3.manager.S3PrefixType;
 import lombok.RequiredArgsConstructor;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.InputStream;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,6 +34,7 @@ public class TransactionService {
     private final AlbumRepository albumRepository;
     private final TrackRepository trackRepository;
     private final TrackMemberRepository trackMemberRepository;
+    private final AwsS3Manager awsS3Manager;
 
     public ListResponse<OriginalTransactionResponseDto> getOriginalTransactions(String uploadAt) {
         List<OriginalTransaction> originalTransactions = originalTransactionRepository.findAllByUploadAt(uploadAt);
@@ -43,6 +50,8 @@ public class TransactionService {
     }
 
     public OriginalTransactionResponseDto saveOriginalTransaction(MultipartFile file, OriginalTransactionRequestDto requestDto) {
+        String uploadAt = requestDto.getUploadAt();
+
         ExcelFileProcessManager excelFileProcessManager = new ExcelFileProcessManager(
                 file,
                 memberRepository,
@@ -56,12 +65,26 @@ public class TransactionService {
             throw new ExcelUploadException(errors);
         }
 
+        String s3Url = awsS3Manager.upload(file, uploadAt + "/" + file.getOriginalFilename(), S3PrefixType.EXCEL);
+        S3Object excelFileS3Object = awsS3Manager.getS3Value(S3PrefixType.EXCEL.getValue() + file.getOriginalFilename());
+
+        Workbook workbook = getWorkBook(excelFileS3Object);
+
         OriginalTransaction originalTransaction = OriginalTransaction.builder()
                 .uploadAt(requestDto.getUploadAt())
                 .fileName(file.getOriginalFilename())
-                .fileUrl("s3")
+                .fileUrl(s3Url)
                 .build();
         originalTransactionRepository.save(originalTransaction);
         return OriginalTransactionResponseDto.fromWithWarning(originalTransaction, excelFileProcessManager.getWarnings());
+    }
+
+    private Workbook getWorkBook(S3Object s3Object) {
+        try (InputStream inputStream = s3Object.getObjectContent()) {
+            return WorkbookFactory.create(inputStream);
+        } catch (Exception e) {
+            // 처리 중 발생한 예외 처리
+            throw new RuntimeException("Error while reading Excel file from S3", e);
+        }
     }
 }
