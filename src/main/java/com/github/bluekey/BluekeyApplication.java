7978f98d705ef48ffc9a710c534a855893040754
oereo <dlstpgns0406@gmail.com>
2023-08-20 21:30:43 +0900

    feat: started method를 통해 timeZone settings 값 적용

--- src/main/java/com/github/bluekey/BluekeyApplication.java
+++ src/main/java/com/github/bluekey/BluekeyApplication.java
@@ -4,12 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication(exclude = {
         org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
         org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
         org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
 })
 public class BluekeyApplication {
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
     public static void main(String[] args) {
         SpringApplication.run(BluekeyApplication.class, args);
     }
