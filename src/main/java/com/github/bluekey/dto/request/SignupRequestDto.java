f71284d513e3eaf2f66ae102e09202bbd04f78a2
oereo <dlstpgns0406@gmail.com>
2023-08-20 20:39:00 +0900

    refactor: SignupRequestDto 생성자를 private으로 변경

--- src/main/java/com/github/bluekey/dto/request/SignupRequestDto.java
+++ src/main/java/com/github/bluekey/dto/request/SignupRequestDto.java
@@ -3,28 +3,27 @@ package com.github.bluekey.dto.request;
 import com.github.bluekey.entity.member.Member;
 import com.github.bluekey.entity.member.MemberRole;
 import io.swagger.v3.oas.annotations.media.Schema;
-import javax.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@Builder
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Schema(description = "회원가입 요청")
 public class SignupRequestDto {
 	// TODO: Validation 추가
 	@Schema(description = "회원 Email", example = "bluekey1@gmail.com")
-	private final String email;
+	private String email;
 	@Schema(description = "회원 id", example = "bluekey1")
-	private final String loginId;
+	private String loginId;
 	@Schema(description = "회원 닉네임", example = "블루키1")
-	private final String nickname;
+	private String nickname;
 	@Schema(description = "회원 비밀번호", example = "blueblue123")
-	private final String password;
+	private String password;
 
-	public SignupRequestDto(String email, String loginId, String nickname, String password) {
+	@Builder
+	private SignupRequestDto(String email, String loginId, String nickname, String password) {
 		this.email = email;
 		this.loginId = loginId;
 		this.nickname = nickname;
