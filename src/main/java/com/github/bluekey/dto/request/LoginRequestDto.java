3bee78e615e54c6237cfb5278f7f01cdce348d49
oereo <dlstpgns0406@gmail.com>
2023-08-20 22:43:06 +0900

    refactor: LoginRequestDto 생성 방식 변경

--- src/main/java/com/github/bluekey/dto/request/LoginRequestDto.java
+++ src/main/java/com/github/bluekey/dto/request/LoginRequestDto.java
@@ -1,17 +1,20 @@
 package com.github.bluekey.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 @Getter
-@RequiredArgsConstructor
-@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Schema(description = "로그인 요청")
 public class LoginRequestDto {
 	@Schema(description = "로그인 ID", example = "bluekey1")
-	private final String loginId;
+	private String loginId;
 	@Schema(description = "비밀번호", example = "blueblue123")
-	private final String password;
+	private String password;
+
+	@Builder
+	public LoginRequestDto(String loginId, String password) {
+		this.loginId = loginId;
+		this.password = password;
+	}
 }
