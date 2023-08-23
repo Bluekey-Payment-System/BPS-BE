f36dea7a0d4d62aa085a537f205f51bbe499e205
oereo <dlstpgns0406@gmail.com>
2023-08-24 04:45:48 +0900

    refactor: S3 object 가져오는 key 값 수정

--- src/main/java/com/github/bluekey/service/transaction/TransactionService.java
+++ src/main/java/com/github/bluekey/service/transaction/TransactionService.java
@@ -92,7 +92,7 @@ public class TransactionService {
         List<OriginalTransaction> originalTransactions = originalTransactionRepository.findAllByIsCompletedFalseAndIsRemovedFalse();
         for (OriginalTransaction originalTransaction: originalTransactions) {
             String s3Key = awsS3Manager.getS3Key(originalTransaction.getFileUrl(), S3PrefixType.EXCEL);
-            S3Object s3Object = excelUploadUtil.getExcel(S3PrefixType.EXCEL.getValue() + s3Key);
+            S3Object s3Object = excelUploadUtil.getExcel(s3Key);
             Workbook workbook = getWorkBook(s3Object);
             workbooks.put(workbook, originalTransaction);
         }
