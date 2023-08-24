8efc638ea8f70f194c8f98921c727ddd4953fffd
oereo <dlstpgns0406@gmail.com>
2023-08-24 17:30:42 +0900

    refactor: MusicDistributorType에서 3.14 엑셀파일 name 검증 정규표현식 수정

--- src/main/java/com/github/bluekey/processor/type/MusicDistributorType.java
+++ src/main/java/com/github/bluekey/processor/type/MusicDistributorType.java
@@ -1,12 +1,13 @@
 package com.github.bluekey.processor.type;
 
 import com.github.bluekey.processor.provider.AtoDistributorExcelFileProvider;
+import com.github.bluekey.processor.provider.ThreePointOneFourDistributorExcelProvider;
 import lombok.Getter;
 
 @Getter
 public enum MusicDistributorType {
     ATO("L2110A11_\\d{6}$", AtoDistributorExcelFileProvider.class),
-    THREE_POINT_ONE_FOUR("^\\d{6}-[a-zA-Z0-9]+$", AtoDistributorExcelFileProvider.class),
+    THREE_POINT_ONE_FOUR("^^\\d{6}-\\p{IsHangul}+$", ThreePointOneFourDistributorExcelProvider.class),
     MAFIA("마피아뮤직_\\d{6}\\$", AtoDistributorExcelFileProvider.class);
 
     private final String pattern;
