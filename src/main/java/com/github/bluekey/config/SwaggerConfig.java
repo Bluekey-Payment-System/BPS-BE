d93e43446cd6b2543d877a8568bf261fb5453fae
eunbi9n <gombi9_9@naver.com>
2023-08-25 22:50:33 +0900

    fix: swagger 인증 수정

--- src/main/java/com/github/bluekey/config/SwaggerConfig.java
+++ src/main/java/com/github/bluekey/config/SwaggerConfig.java
@@ -1,27 +1,32 @@
 package com.github.bluekey.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(info = @Info(title = "Bluekey 프로젝트 API", version = "v1"), servers = {@Server(url = "/", description = "Default Server Url")})
-@SecurityScheme(
-		name = "Bearer Authentication",
-		type = SecuritySchemeType.HTTP,
-		bearerFormat = "JWT",
-		scheme = "bearer"
-)
 public class SwaggerConfig {
 	//TODO: 인증에 대한 부분 추가
 	@Bean
 	public OpenAPI getOpenAPI() {
-		return new OpenAPI().components(new Components());
+		final String securitySchemeName = "bearerAuth";
+		SecurityScheme securityScheme = new SecurityScheme()
+				.type(SecurityScheme.Type.HTTP)
+				.scheme("bearer")
+				.bearerFormat("JWT")
+				.in(SecurityScheme.In.HEADER)
+				.name("Authorization");
+		return new OpenAPI()
+				.addSecurityItem(new SecurityRequirement()
+						.addList(securitySchemeName))
+				.components(new Components()
+						.addSecuritySchemes(securitySchemeName, securityScheme));
 	}
 }
