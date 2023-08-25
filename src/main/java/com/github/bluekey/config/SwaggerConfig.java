83053c65c6326f42e32afaa9bec8591b3254948f
eunbi9n <gombi9_9@naver.com>
2023-08-25 18:01:40 +0900

    feat: swagger Url 설정 추가

--- src/main/java/com/github/bluekey/config/SwaggerConfig.java
+++ src/main/java/com/github/bluekey/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.github.bluekey.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import lombok.RequiredArgsConstructor;
@@ -9,7 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "Bluekey 프로젝트 API", version = "v1"))
+@OpenAPIDefinition(info = @Info(title = "Bluekey 프로젝트 API", version = "v1"), servers = {@Server(url = "/", description = "Default Server Url")})
 @RequiredArgsConstructor
 public class SwaggerConfig {
 	//TODO: 인증에 대한 부분 추가
