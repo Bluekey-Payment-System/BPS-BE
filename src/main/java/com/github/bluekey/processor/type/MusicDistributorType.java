e2b113428c9a312a59d115701c19dc842440e763
oereo <dlstpgns0406@gmail.com>
2023-08-25 23:22:53 +0900

    refactor: MusicDistributorType의 MAFIA field에 대해 cls 수정

--- src/main/java/com/github/bluekey/processor/type/MusicDistributorType.java
+++ src/main/java/com/github/bluekey/processor/type/MusicDistributorType.java
@@ -1,6 +1,7 @@
 package com.github.bluekey.processor.type;
 
 import com.github.bluekey.processor.provider.AtoDistributorExcelFileProvider;
+import com.github.bluekey.processor.provider.MafiaDistributorExcelFileProvider;
 import com.github.bluekey.processor.provider.ThreePointOneFourDistributorExcelFileProvider;
 import lombok.Getter;
 
@@ -8,7 +9,7 @@ import lombok.Getter;
 public enum MusicDistributorType {
     ATO("L2110A11_\\d{6}$", AtoDistributorExcelFileProvider.class),
     THREE_POINT_ONE_FOUR("^^\\d{6}-\\p{IsHangul}+$", ThreePointOneFourDistributorExcelFileProvider.class),
-    MAFIA("마피아뮤직_\\d{6}\\$", AtoDistributorExcelFileProvider.class);
+    MAFIA("마피아_\\d{6}_\\p{IsHangul}+$", MafiaDistributorExcelFileProvider.class);
 
     private final String pattern;
     private final Class<?> cls;
