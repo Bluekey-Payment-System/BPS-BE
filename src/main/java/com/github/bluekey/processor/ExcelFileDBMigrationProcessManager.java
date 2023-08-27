cd4c3abfc6e65075017936dca2567a13f252aa5e
oereo <dlstpgns0406@gmail.com>
2023-08-27 18:29:37 +0900

    feat: mafiaWorkbookProcess, migrateMafiaExcelDataToDB method 추가

--- src/main/java/com/github/bluekey/processor/ExcelFileDBMigrationProcessManager.java
+++ src/main/java/com/github/bluekey/processor/ExcelFileDBMigrationProcessManager.java
@@ -6,6 +6,7 @@ import com.github.bluekey.entity.transaction.ExcelDistributorType;
 import com.github.bluekey.entity.transaction.OriginalTransaction;
 import com.github.bluekey.entity.transaction.Transaction;
 import com.github.bluekey.processor.type.AtoExcelColumnType;
+import com.github.bluekey.processor.type.MafiaExcelColumnType;
 import com.github.bluekey.processor.type.ThreePointOneFourExcelColumnType;
 import com.github.bluekey.repository.member.MemberRepository;
 import com.github.bluekey.repository.track.TrackMemberRepository;
@@ -17,6 +18,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.ss.usermodel.*;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 @Getter
@@ -26,9 +29,12 @@ import java.util.*;
 public class ExcelFileDBMigrationProcessManager implements ProcessManager {
     private static final int ATO_ACTIVE_EXCEL_SHEET_INDEX = 1;
     private static final int THREE_POINT_ONE_FOUR_ACTIVE_EXCEL_SHEET_INDEX = 2;
+    private static final int MAFIA_ACTIVE_EXCEL_SHEET_INDEX = 0;
 
     private static final int ATO_DATA_ROW_START_INDEX = 5;
     private static final int THREE_POINT_ONE_FOUR_DATA_ROW_START_INDEX = 4;
+    private static final int MAFIA_DATA_ROW_START_INDEX = 3;
+    private static final int MAFIA_DATA_HEADER_INDEX = 2;
 
     private final MemberRepository memberRepository;
     private final TrackMemberRepository trackMemberRepository;
@@ -50,12 +56,59 @@ public class ExcelFileDBMigrationProcessManager implements ProcessManager {
                 Sheet sheet = distributorWorkbook.getSheetAt(THREE_POINT_ONE_FOUR_ACTIVE_EXCEL_SHEET_INDEX);
                 threePointOneFourWorkbookProcess(sheet, workbook.getValue());
             }
+//            if (distributorType.equals(ExcelDistributorType.MAFIA)) {
+//                Sheet sheet = distributorWorkbook.getSheetAt(MAFIA_ACTIVE_EXCEL_SHEET_INDEX);
+//                mafiaWorkbookProcess(sheet, workbook.getValue());
+//
+//            }
         }
 
     }
 
     public void updateWorkbooks(Map<Workbook, OriginalTransaction> workbooks) {
         this.workbooks = workbooks;
+    }
+
+    private void mafiaWorkbookProcess(Sheet sheet, OriginalTransaction originalTransaction) {
+        String file = originalTransaction.getFileName();
+        String fileName = file.split("\\.")[0];
+        String transactionAt = fileName.split("_")[1];
+        Row header = sheet.getRow(MAFIA_DATA_HEADER_INDEX);
+        int columnIndex = getTransactionAt(header, transactionAt);
+
+        for (int i = MAFIA_DATA_ROW_START_INDEX; i <= sheet.getLastRowNum(); i++) {
+            Row row = sheet.getRow(i);
+            migrateMafiaExcelDataToDB(row, originalTransaction, columnIndex);
+        }
+    }
+
+    private void migrateMafiaExcelDataToDB(Row row, OriginalTransaction originalTransaction, int transactionAt) {
+
+        DataFormatter dataFormatter = new DataFormatter();
+
+        Cell albumCell = row.getCell(MafiaExcelColumnType.ALBUM_NAME.getIndex());
+        Cell trackCell = row.getCell(MafiaExcelColumnType.TRACK_NAME.getIndex());
+        Cell amountCell = row.getCell(transactionAt);
+
+        log.info("albumCell = {}", albumCell.getStringCellValue());
+        log.info("trackCell = {}", trackCell.getStringCellValue());
+        log.info("amountCell = {}", amountCell.getNumericCellValue());
+
+    }
+
+    private int getTransactionAt(Row row, String transactionAt) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+        for (Cell cell: row) {
+            if (cell.getColumnIndex() >=4 && cell.getColumnIndex() <=16) {
+                LocalDate date1 = cell.getLocalDateTimeCellValue().toLocalDate();
+                LocalDate date2 = LocalDate.parse(transactionAt + "01", formatter); // Assuming input2 is in yyyyMM format
+                if (date1.equals(date2)) {
+                    return cell.getColumnIndex();
+                }
+            }
+
+        }
+        throw new RuntimeException("날짜 데이터 파싱 에러");
     }
 
     private void threePointOneFourWorkbookProcess(Sheet sheet, OriginalTransaction originalTransaction) {
