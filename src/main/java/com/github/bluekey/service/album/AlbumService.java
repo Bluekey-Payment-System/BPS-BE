2e13117a8622aea75c5c9f98fee2ece44b9dd75f
eunbi9n <gombi9_9@naver.com>
2023-08-28 02:43:07 +0900

    feat: 앨범 트랙 조회 API 구현

--- src/main/java/com/github/bluekey/service/album/AlbumService.java
+++ src/main/java/com/github/bluekey/service/album/AlbumService.java
@@ -1,11 +1,17 @@
 package com.github.bluekey.service.album;
 
 import com.github.bluekey.dto.album.NewAlbumInfoDto;
+import com.github.bluekey.dto.artist.ArtistInfoDto;
+import com.github.bluekey.dto.common.MemberBaseDto;
 import com.github.bluekey.dto.response.album.AlbumIdResponseDto;
 import com.github.bluekey.dto.response.album.AlbumResponseDto;
+import com.github.bluekey.dto.response.album.AlbumTrackListResponseDto;
+import com.github.bluekey.dto.track.TrackArtistsDto;
+import com.github.bluekey.dto.track.TrackInfoListDto;
 import com.github.bluekey.entity.album.Album;
 import com.github.bluekey.entity.member.Member;
 import com.github.bluekey.entity.track.Track;
+import com.github.bluekey.entity.track.TrackMember;
 import com.github.bluekey.exception.BusinessException;
 import com.github.bluekey.exception.ErrorCode;
 import com.github.bluekey.exception.member.MemberNotFoundException;
@@ -14,7 +20,9 @@ import com.github.bluekey.repository.member.MemberRepository;
 import com.github.bluekey.repository.track.TrackRepository;
 import com.github.bluekey.s3.manager.S3PrefixType;
 import com.github.bluekey.util.ImageUploadUtil;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -62,6 +70,51 @@ public class AlbumService {
 		tracks.forEach(track -> track.remove());
 
 		return AlbumIdResponseDto.builder().albumId(albumId).build();
+	}
+
+	@Transactional(readOnly = true)
+	public AlbumTrackListResponseDto getAlbumTrackList(Long albumId) {
+		Album album = albumRepository.findAlbumByIdAndIsRemovedFalse(albumId)
+				.orElseThrow(() -> new BusinessException(ErrorCode.ALBUM_NOT_FOUND));
+
+		ArtistInfoDto artist = null;
+		if (album.getArtistId() != null) {
+			Member member = memberRepository.findMemberByIdAndIsRemovedFalse(album.getArtistId())
+					.orElseThrow(MemberNotFoundException::new);
+			artist = ArtistInfoDto.from(member);
+		}
+
+		List<Track> tracks = trackRepository.findAllByAlbumIdAndIsRemovedFalse(albumId);
+
+		List<TrackInfoListDto> trackInfoList = tracks
+				.stream()
+				.map(track -> {
+					List<TrackArtistsDto> memberList = track.getTrackMembers()
+							.stream()
+							.map(member -> {
+								if (member.getMemberId() != null) {
+									Member memberInfo = memberRepository
+											.findMemberByIdAndIsRemovedFalse(member.getMemberId())
+											.orElseThrow(MemberNotFoundException::new);
+									return TrackArtistsDto.from(memberInfo);
+								} else {
+									return TrackArtistsDto.from(member);
+								}
+							})
+							.collect(Collectors.toList());
+					return TrackInfoListDto.from(track, memberList);
+				})
+				.collect(Collectors.toList());
+
+		return AlbumTrackListResponseDto
+				.builder()
+				.albumId(album.getId())
+				.albumImage(album.getProfileImage())
+				.name(album.getName())
+				.enName(album.getEnName())
+				.tracks(trackInfoList)
+				.artist(artist)
+				.build();
 	}
 
 	private void updateAlbumImage(MultipartFile file, Album album) {
