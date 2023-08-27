cdad40b0dd8cde3f38f73ea011238da32d3aa2e0
eunbi9n <gombi9_9@naver.com>
2023-08-28 01:29:38 +0900

    remove: import 삭제

--- src/main/java/com/github/bluekey/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/github/bluekey/jwt/JwtAuthenticationFilter.java
@@ -1,25 +1,15 @@
 package com.github.bluekey.jwt;
 
-import com.amazonaws.services.ec2.model.transform.CreditSpecificationStaxUnmarshaller;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.jsonwebtoken.ExpiredJwtException;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.List;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.github.bluekey.exception.AuthenticationException;
-import com.github.bluekey.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Slf4j
