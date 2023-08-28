92da324c954c390fa884bfb66486eca91727bf7c
eunbi9n <gombi9_9@naver.com>
2023-08-29 01:38:44 +0900

    fix: 공통 dto로 사용하기 위한 위한 이름 변경 및 자료형 변경

--- src/main/java/com/github/bluekey/dto/common/MonthlyTrendDto.java
+++ src/main/java/com/github/bluekey/dto/common/MonthlyTrendDto.java
@@ -2,27 +2,29 @@ package com.github.bluekey.dto.common;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Schema(description = "월 매출 정보")
-public class MonthlyRevenueDto {
+@Schema(description = "월 매출 + 정산액 or 회사 순이익 정보")
+public class MonthlyTrendDto {
 	@Schema(description = "월", example = "1")
-	private Long month;
+	private Integer month;
 	@Schema(description = "매출", example = "1000000")
-	private Long revenue;
+	private double revenue;
 	@Schema(description = "순수익", example = "1000000")
-	private Long netIncome;
+	private Double netIncome;
+	@Schema(description = "정산액", example = "1000000")
+	private Double settlement;
 
 	@Builder
-	public MonthlyRevenueDto(final Long month, final Long revenue, final Long netIncome) {
+	public MonthlyTrendDto(final Integer month, final double revenue, final Double netIncome,
+			final Double settlement) {
 		this.month = month;
 		this.revenue = revenue;
 		this.netIncome = netIncome;
+		this.settlement = settlement;
 	}
 }
