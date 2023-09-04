28e1ab7cb2c792a96e15ab6495d06e0e507d216c
oereo <dlstpgns0406@gmail.com>
2023-09-04 13:29:51 +0900

    feat: ExcelUploadException에 fileName field 추가
    - 업로드 시 에러가 날 경우 에러를 발생한 엑셀파일명 데이터를 반환

--- src/main/java/com/github/bluekey/exception/transaction/ExcelUploadException.java
+++ src/main/java/com/github/bluekey/exception/transaction/ExcelUploadException.java
@@ -9,10 +9,12 @@ import java.util.List;
 @Getter
 public class ExcelUploadException extends RuntimeException{
     private final ErrorCode errorCode;
+    private final String fileName;
     private final List<ExcelRowException> errors;
 
-    public ExcelUploadException(List<ExcelRowException> errors) {
+    public ExcelUploadException(String fileName, List<ExcelRowException> errors) {
         this.errorCode = ErrorCode.TRANSACTION_INVALID_EXCEL_READER_VALUE;
+        this.fileName = fileName;
         this.errors = errors;
     }
 }
