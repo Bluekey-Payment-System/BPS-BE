75226c23d7f7b9a7651e7dd4a5d82a8e5532b477
oereo <dlstpgns0406@gmail.com>
2023-09-06 17:04:44 +0900

    feat:: findAlbumByIdAndIsRemovedFalseOrElseThrow default method 정의

--- src/main/java/com/github/bluekey/repository/album/AlbumRepository.java
+++ src/main/java/com/github/bluekey/repository/album/AlbumRepository.java
@@ -1,18 +1,23 @@
 package com.github.bluekey.repository.album;
 
 import com.github.bluekey.entity.album.Album;
+import com.github.bluekey.exception.BusinessException;
+import com.github.bluekey.exception.ErrorCode;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface AlbumRepository extends JpaRepository<Album, Long> {
 
     Optional<Album> findAlbumByEnNameIgnoreCase(String name);
+
     Optional<Album> findAlbumByNameIgnoreCase(String name);
+
     Optional<Album> findAlbumByIdAndIsRemovedFalse(Long id);
 
     Page<Album> findAllByIsRemovedFalseOrderByCreatedAtDesc(Pageable pageable);
@@ -23,4 +28,10 @@ public interface AlbumRepository extends JpaRepository<Album, Long> {
             "AND (a.name LIKE %:keyword% OR a.enName LIKE %:keyword%) " +
             "ORDER BY a.createdAt DESC")
     Page<Album> findAllByIsRemovedFalseAndSearchByKeyword(Pageable pageable, @Param("keyword") String keyword);
+
+    default Album findAlbumByIdAndIsRemovedFalseOrElseThrow(Long id) {
+        return this.findAlbumByIdAndIsRemovedFalse(id).orElseThrow(() ->
+                new BusinessException(ErrorCode.ALBUM_NOT_FOUND)
+        );
+    }
 }
