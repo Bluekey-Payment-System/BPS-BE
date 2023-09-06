fc69c68ab2d5a7ddc718e7c1f8af84172efa5035
oereo <dlstpgns0406@gmail.com>
2023-09-06 14:38:44 +0900

    feat: jwt가 invalid한 경우에 대해서 errorMessage 분기 처리 로직 추가

--- src/main/java/com/github/bluekey/config/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/github/bluekey/config/security/jwt/JwtAuthenticationFilter.java
@@ -6,6 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.github.bluekey.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
@@ -16,17 +17,40 @@ import org.springframework.web.filter.OncePerRequestFilter;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-	private final JwtProvider jwtProvider;
-	private static final int BEARER_TOKEN_PREFIX = 7;
+    private static final String EXCEPTION_PROPERTY = "exception";
+    private static final int BEARER_TOKEN_PREFIX = 7;
 
-	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-			throws ServletException, IOException {
-		String token = jwtProvider.resolveToken(request);
-		if (token != null && jwtProvider.validateToken(token)) {
-			Authentication authentication = jwtProvider.getAuthentication(token.substring(BEARER_TOKEN_PREFIX));
-			SecurityContextHolder.getContext().setAuthentication(authentication);
-		}
-		filterChain.doFilter(request, response);
-	}
+    private final JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String token = jwtProvider.resolveToken(request);
+        if (token != null && jwtProvider.validateToken(token).equals(JwtValidationType.VALID_JWT)) {
+            Authentication authentication = jwtProvider.getAuthentication(token.substring(BEARER_TOKEN_PREFIX));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } else {
+            request.setAttribute("UnAuthorization", jwtProvider.validateToken(token));
+            setErrorMessage(jwtProvider.validateToken(token), request);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private void setErrorMessage(JwtValidationType jwtValidationType, HttpServletRequest request) {
+        if (jwtValidationType.equals(JwtValidationType.EMPTY_JWT)) {
+            request.setAttribute(EXCEPTION_PROPERTY, ErrorCode.EMPTY_JWT);
+        }
+        if (jwtValidationType.equals(JwtValidationType.EXPIRED_JWT)) {
+            request.setAttribute(EXCEPTION_PROPERTY, ErrorCode.EXPIRED_JWT);
+        }
+        if (jwtValidationType.equals(JwtValidationType.INVALID_JWT)) {
+            request.setAttribute(EXCEPTION_PROPERTY, ErrorCode.INVALID_JWT);
+        }
+        if (jwtValidationType.equals(JwtValidationType.UNSUPPORTED_JWT)) {
+            request.setAttribute(EXCEPTION_PROPERTY, ErrorCode.UNSUPPORTED_JWT);
+        }
+        if(jwtValidationType.equals(JwtValidationType.INVALID_JWT_SIGNATURE)) {
+            request.setAttribute(EXCEPTION_PROPERTY, ErrorCode.INVALID_JWT_SIGNATURE);
+        }
+    }
 }
