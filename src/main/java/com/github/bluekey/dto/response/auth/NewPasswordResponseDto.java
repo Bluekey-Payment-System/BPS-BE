02ffd888464b66f32dcf0494033020dde8730730
eunbi9n <gombi9_9@naver.com>
2023-09-07 18:38:16 +0900

    feat: 비밀번호 재발급을 위한 dto 생성

---
+++ src/main/java/com/github/bluekey/dto/response/auth/NewPasswordResponseDto.java
@@ -0,0 +1,19 @@
+package com.github.bluekey.dto.response.auth;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class NewPasswordResponseDto {
+	@Schema(description = "새로운 비밀번호", example = "qwerty123")
+	private String newPassword;
+
+	@Builder
+	public NewPasswordResponseDto(final String newPassword) {
+		this.newPassword = newPassword;
+	}
+}
