37f33c21e85d84f2b346e9beb61ec1d6786636da
eunbi9n <gombi9_9@naver.com>
2023-09-19 00:49:49 +0900

    refactor: yml로 cron-config 관리할 수 있도록 변경

--- src/main/java/com/github/bluekey/config/ScheduleJobConfig.java
+++ src/main/java/com/github/bluekey/config/ScheduleJobConfig.java
@@ -18,16 +18,9 @@ import static org.quartz.JobBuilder.newJob;
 public class ScheduleJobConfig {
     private static final String EXCEL_TO_DATABASE_CRON_CONFIG = "0 1 1 * *";
     private static final String EXCEL_TO_DATABASE_CRON_CONFIG_LOCAL_VER = "0 3 * * * ?";
-    // 매월 1일 오전 9시에 실행
-    private static final String EMAIL_SEND_CRON_CONFIG = "0 9 1 * *";
-    // 매월 1일 오전 9시에 실행
-    private static final String EMAIL_SEND_CRON_CONFIG_DEV_VER = "0 9 1 * *";
-    // 실행 후 10분마다 실행
-    private static final String EMAIL_SEND_CRON_CONFIG_LOCAL_VER = "0 */1 * * * ?";
-    private static final String PRODUCTION = "prod";
-    private static final String DEV = "dev";
-    @Value("${spring.config.activate.on-profile}")
-    private String profile;
+
+    @Value("${bluekey.schedule.cron-config}")
+    private String cronConfig;
     private final Scheduler scheduler;
 
     @PostConstruct
@@ -35,12 +28,7 @@ public class ScheduleJobConfig {
         JobDetail detail = runJobDetail(EmailSendJob.class, new HashMap<>());
 
         try {
-            if (profile.equals(PRODUCTION))
-                scheduler.scheduleJob(detail, runJobTrigger(EMAIL_SEND_CRON_CONFIG));
-            else if (profile.equals(DEV))
-                scheduler.scheduleJob(detail, runJobTrigger(EMAIL_SEND_CRON_CONFIG_DEV_VER));
-            else
-                scheduler.scheduleJob(detail, runJobTrigger(EMAIL_SEND_CRON_CONFIG_LOCAL_VER));
+            scheduler.scheduleJob(detail, runJobTrigger(cronConfig));
         } catch (SchedulerException e) {
             e.printStackTrace();
         }
