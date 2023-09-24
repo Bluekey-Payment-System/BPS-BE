11e193126e6987518a946adcfb2f4d64326f9cd9
eunbi9n <gombi9_9@naver.com>
2023-09-24 17:56:34 +0900

    feat: 권한 요청에 대해 super_admin이 승인하는 서비스 로직 구현

--- src/main/java/com/github/bluekey/repository/notification/MemberRequestAuthorityRepository.java
+++ src/main/java/com/github/bluekey/repository/notification/MemberRequestAuthorityRepository.java
@@ -1,9 +1,19 @@
 package com.github.bluekey.repository.notification;
 
 import com.github.bluekey.entity.notification.MemberRequestAuthority;
+import com.github.bluekey.exception.BusinessException;
+import com.github.bluekey.exception.ErrorCode;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRequestAuthorityRepository extends
 		JpaRepository<MemberRequestAuthority, Long> {
+	Optional<MemberRequestAuthority> findMemberRequestAuthorityByMemberIdAndRequestAuthorityId(Long memberId, Long requestAuthorityId);
 
+	default void findMemberRequestAuthorityByMemberIdAndRequestAuthorityIdOrElseThrow(Long memberId, Long requestAuthorityId) {
+		this.findMemberRequestAuthorityByMemberIdAndRequestAuthorityId(memberId, requestAuthorityId)
+				.orElseThrow(() ->
+						new BusinessException(ErrorCode.AUTHORIZATION_FAILED)
+				);
+	}
 }
