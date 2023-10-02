5133f56dae306ad4d6dc4244e4a229871a6d56e9
oereo <dlstpgns0406@gmail.com>
2023-10-02 20:53:54 +0900

    feat: Service layer versioning

---
+++ src/main/java/com/github/bluekey/service/dashboard/v2/TopTrackDashBoardServiceV2.java
@@ -0,0 +1,9 @@
+package com.github.bluekey.service.dashboard.v2;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TopTrackDashBoardServiceV2 {
+}
