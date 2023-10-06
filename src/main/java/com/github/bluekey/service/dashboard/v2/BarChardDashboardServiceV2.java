4f6346bc69fd28ed169638197a6a27f8225acae4
oereo <dlstpgns0406@gmail.com>
2023-10-06 16:24:16 +0900

    feat: getAlbumMonthlyTrends service method 추가와 BarChartDashboardServiceV2 v2 class 생성

--- src/main/java/com/github/bluekey/service/dashboard/v2/BarChardDashboardServiceV2.java
+++
@@ -1,9 +0,0 @@
-package com.github.bluekey.service.dashboard.v2;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class BarChardDashboardServiceV2 {
-}
